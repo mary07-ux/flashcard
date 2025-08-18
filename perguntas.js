04ca82384834de1803749f93e960e2781aa26623
mary07-ux <mary.rocha@escola.pr.gov.br>
2025-08-18 13:38:07 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'História',
+    'Quem foi Dom Pedro I?',
+    'Dom Pedro I foi o primeiro imperador do Brasil, proclamando a independência do país em 1822.'
+);
+
+criaCartao(
+    'História',
+    'O que foi a Revolução Industrial?',
+    'A Revolução Industrial foi um período de grandes transformações econômicas, tecnológicas e sociais, que começou no final do século XVIII na Inglaterra.'
+);
+
+criaCartao(
+    'História',
+    'O que aconteceu na Guerra do Paraguai?',
+    'A Guerra do Paraguai (1864-1870) foi um conflito entre o Paraguai e uma aliança formada por Brasil, Argentina e Uruguai, resultando em grandes perdas humanas e econômicas para o Paraguai.'
+);
+
+criaCartao(
+    'História',
+    'Quem foi Tiradentes?',
+    'Tiradentes foi um líder da Inconfidência Mineira, movimento de luta pela independência do Brasil, e é considerado um mártir da independência.'
+);
+
+criaCartao(
+    'História',
+    'O que foi o golpe de 1964 no Brasil?',
+    'O golpe de 1964 foi um movimento militar que depôs o presidente João Goulart e instaurou uma ditadura militar no Brasil, que durou até 1985.'
+);
